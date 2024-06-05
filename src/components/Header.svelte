b55954682fbb04e5add6da32e18f9b98a1a4705d
Irtiza194 <aliirtiza44@gmail.com>
2024-06-05 19:05:35 +0100

    first commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,20 @@
+<header class="navbar gap-5 px-5 py-5 bg-base-300 shadow-md fixed">
+	<div class="navbar-start">
+		<h1 class="font-serif text-3xl cursor-default max-md:text-xl">Quranic</h1>
+	</div>
+	<div class="navbar-end">
+		<div class="dropdown dropdown-end">
+			<div tabindex="0" role="button" class="btn btn-outline">
+				<i class="ri-menu-line"></i>
+			</div>
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<ul
+				tabindex="0"
+				class="dropdown-content z-[1] menu p-2 shadow-md bg-base-200 rounded-box w-52"
+			>
+				<li><a href="/"><i class="ri-question-line"></i>About Us</a></li>
+				<li><a href="/"><i class="ri-refresh-line"></i>Releases</a></li>
+			</ul>
+		</div>
+	</div>
+</header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Surahs from '../components/Surahs.svelte';
+	import Header from '../components/Header.svelte';
+</script>
+
+<Header></Header>
+<Surahs></Surahs>
