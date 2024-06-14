a12dc468da8ab6466b6213411926e01e3377dfd8
Irtiza194 <aliirtiza44@gmail.com>
2024-06-14 13:20:43 +0100

    Addition of the player page

--- src/routes/player/+page.svelte
+++ src/routes/player/+page.svelte
@@ -3,3 +3,11 @@
 </script>
 
 <SecondaryHeader></SecondaryHeader>
+<main class="hero min-h-screen">
+	<div class="hero-content">
+		<div role="alert" class="alert alert-error">
+            <i class="ri-error-warning-line text-2xl"></i>
+			<h1>This page is under construction</h1>
+		</div>
+	</div>
+</main>
